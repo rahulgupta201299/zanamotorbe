e61dcd0090aaf458f60e9d279118af75ccf512ad
harshdedhia12 <harsh.dedhia12@gmail.com>
2025-11-28 16:16:07 +0530

    Fixing of getting profile api via mobile number and cors issue

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const connectDB = require('./config/database');
+const cors  = require('cors')
 
 const app = express();
 app.use(cors());
