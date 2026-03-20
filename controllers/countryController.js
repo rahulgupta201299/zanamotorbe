46321cd7c08adff678c9d701362f80d376ddf6ef
harshdedhia12 <harsh.dedhia12@gmail.com>
2026-03-20 17:35:58 +0530

    Added changes of sending values in currencies api

--- controllers/countryController.js
+++ controllers/countryController.js
@@ -1,5 +1,6 @@
 const countries = require('../utils/isdCodes');
 const currencies = require('../utils/currencyList');
+const { fetchExchangeRates } = require('../utils/exchangeRate');
 
 exports.getIsdCodes = (req, res) => {
     res.status(200).json({
@@ -8,9 +9,22 @@ exports.getIsdCodes = (req, res) => {
     });
 };
 
-exports.getCurrencies = (req, res) => {
-    res.status(200).json({
-        success: true,
-        data: currencies
-    });
+exports.getCurrencies = async (req, res) => {
+    try {
+        const rates = await fetchExchangeRates();
+        const currenciesWithRates = currencies.map(currency => ({
+            ...currency,
+            exchangeRate: rates[currency.code] || 1
+        }));
+        
+        res.status(200).json({
+            success: true,
+            data: currenciesWithRates
+        });
+    } catch (error) {
+        res.status(500).json({
+            success: false,
+            message: 'Error fetching currencies'
+        });
+    }
 };
