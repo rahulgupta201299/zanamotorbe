90504df20baab237e8ffae6f1cc376b8e416a84b
harshdedhia12 <harsh.dedhia12@gmail.com>
2026-07-10 19:26:53 +0530

    Added admin order create and update API and updated admin order list and csv download api

--- models/Order.js
+++ models/Order.js
@@ -132,7 +132,13 @@ const orderSchema = new mongoose.Schema({
     },
     paymentMethod: {
         type: String,
-        enum: ['card', 'upi', 'netbanking', 'cod', 'wallet', 'online'],
+        enum: ['cod', 'online'],
+        required: true
+    },
+    paymentType: {
+        type: String,
+        enum: ['razorpay', 'upi'],
+        default: "razorpay",
         required: true
     },
     paymentStatus: {
@@ -153,6 +159,9 @@ const orderSchema = new mongoose.Schema({
         type: String
     },
     razorpaySignature: {
+        type: String
+    },
+    adminCapturedPaymentId: {
         type: String
     },
     orderStatus: {
@@ -185,6 +194,10 @@ const orderSchema = new mongoose.Schema({
     },
     notes: {
         type: String
+    },
+    isAdminCreated: {
+        type: Boolean,
+        default: false
     },
     // Original cart reference
     originalCartId: {
--- routes/order.js
+++ routes/order.js
@@ -6,6 +6,10 @@ router.get('/user/:phoneNumber', orderController.getOrdersByPhone);
 router.get('/admin/all', orderController.getAdminAllOrders);
 router.get('/admin/all/download', orderController.downloadAdminAllOrdersCsv);
 router.get('/admin/stats', orderController.getAdminOrderStats);
+
+router.post('/admin/create', orderController.adminCreateOrder);
+router.post('/admin/update/:orderId', orderController.adminUpdateOrder);
+
 router.get('/:orderId', orderController.getOrderById);
 router.get('/track-order/:orderId', orderController.trackOrderByOrderId);
 
